097db75ca449e120a204bd6b47326b642627fac7
Angara-Marie <angaramarie@gmail.com>
2022-07-12 15:09:17 +0300

    first commit

---
+++ assignment1.js
@@ -0,0 +1,10 @@
+function Purchase(name, price, quantity){
+    this.name = name
+    this.price = price
+    this.quantity = quantity
+}
+let fruit1 = new Purchase("Orange", 30, 2)
+function calculateFruitCost(){
+    let total_cost = name * quantity
+    console.log();
+}
