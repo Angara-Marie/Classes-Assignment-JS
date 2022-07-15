70942c38cc9c42a16eb9e0e092d0960d61ec97cb
Angara-Marie <angaramarie@gmail.com>
2022-07-15 07:40:21 +0300

    added KioskCacl

--- assignment1.js
+++ assignment1.js
@@ -1,10 +1,24 @@
-function Purchase(name, price, quantity){
-    this.name = name
+function Purchase( price){
     this.price = price
-    this.quantity = quantity
+    this.calculateFruitCost = function(name,quantity){
+        var totalPrice = this.price * quantity
+        return `${quantity} ${name} for kes ${totalPrice}`
+    }
+
 }
-let fruit1 = new Purchase("Orange", 30, 2)
-function calculateFruitCost(){
-    let total_cost = name * quantity
-    console.log();
+var orange = new Purchase(20)
+console.log(orange.calculateFruitCost('orange', 2));
+
+class KioskCalc{
+    constructor(fruit, quantity){
+        this.fruit = fruit
+        this.quantity = quantity
+        this.fruitPriceList = {"orange": 20, "apple": 30, "pear": 20 };
+        this.totalCost = function () {
+        return `${quantity} ${fruit} for KES ${quantity * this.price}`
+        }
+    }
 }
+    KioskCalc.prototype.price = 40;
+    var shop = new KioskCalc('apple',7);
+    console.log(shop.totalCost());
